61a97370ae9aa416977b78e3cafb6e4ffd001ab2
KenasMonstro <163136743+KenasMonstro@users.noreply.github.com>
2024-03-12 09:39:39 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://s2-ge.glbimg.com/SDQCkpYCU4C_R6Ceswb5YC6ptDQ=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/C/4/WC166yT0uMa85VkUrLTQ/15br-social-lineup-social-1920x1080-685054651.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://f.i.uol.com.br/fotografia/2020/05/18/15898209045ec2bde8e4e25_1589820904_3x2_md.jpg";
+}
